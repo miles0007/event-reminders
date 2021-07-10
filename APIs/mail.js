8f9a016773c919e20dd59af5e5750391a00a0596
miles0007 <milesstonner@gmail.com>
2021-07-10 18:09:55 +0530

    mail change

--- APIs/mail.js
+++ APIs/mail.js
@@ -2,7 +2,9 @@
 const nodemailer = require("nodemailer");
 
 let mailTransporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: "milesstonner@gmail.com",
     pass: "intotheverse",
