e8ec8a3e4912983621b913d749291cfc12d5ff6a
miles0007 <milesstonner@gmail.com>
2021-07-10 17:23:28 +0530

    time fixes

--- cron/cron.js
+++ cron/cron.js
@@ -5,3 +5,8 @@ const { birthdayEvents } = require('../APIs/utils');
 cron.schedule("35 7 * * *", function () {
   birthdayEvents();
 });
+
+
+cron.schedule("30 17 * * *", function() {
+  birthdayEvents();
+})
